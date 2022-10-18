6bb3097f1c4f88e06d616fa422e949e5b54b6750
kemijones09@gmail.com <kemijones09@gmail.com>
2022-10-18 19:35:30 -0400

    modified app code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,10 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		String name = "Kemi";
+
+		System.out.println(name);
 	}
 
 }
